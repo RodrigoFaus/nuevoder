982ed2533ca3cee156ed63cb08257d6c36175b6f
rfaus <rodridevelop@hotmail.com>
2022-07-22 12:39:35 -0300

    inciando

---
+++ js/preload.js
@@ -0,0 +1,9 @@
+window.addEventListener ("load", () => {
+    const preloader = document.querySelector(".preloader")
+    preloader.style.opacity = 1
+    preloader.style.visibility = "hidden"  
+
+}  )
+
+
+
